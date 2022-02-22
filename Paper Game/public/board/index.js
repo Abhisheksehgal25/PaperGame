6b8d8d006f03dc9fb82c797f4e75e74a8a8290f1
Abhisheksehgal25 <90157277+Abhisheksehgal25@users.noreply.github.com>
2022-02-22 10:39:42 +0530

    Add files via upload

---
+++ Paper Game/public/board/index.js
@@ -0,0 +1,9 @@
+document.getElementById("B1").onclick = function () {
+    location.href = "Sudoku/sudoku.html";
+};
+document.getElementById("B2").onclick = function () {
+    location.href = "Stone-Paper-Scissor/Stone-paper-scissor.html";
+};
+document.getElementById("B3").onclick = function () {
+    location.href = "TicTacToe/tictactoe.html";
+};
